6d67b7e727d863da6c25497e3cc945356cf80fdf
saeed <saeedfa20201@gmail.com>
2022-04-22 22:40:12 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "./stories/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button label={"Button"} />
+    </div>
+  );
+}
+
+export default App;
